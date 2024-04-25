6c491868c14ee0db83eddb403d9a65803aeca7d7
avfernandez03 <167803504+avfernandez03@users.noreply.github.com>
2024-04-25 13:42:23 +0200

    Added basic search engine

--- src/index.js
+++ src/index.js
@@ -1,2 +1,11 @@
 
-alert(`Hello world!`);
+function handleSearch(event) {
+    event.preventDefault();
+
+    let searchInput= document.querySelector("#search-form-input");
+    let cityElement= document.querySelector("h1");
+    cityElement.innerHTML= searchInput.value;
+}
+
+let searchInputElement = document.querySelector("#search-form");
+searchInputElement.addEventListener("submit", handleSearch);
