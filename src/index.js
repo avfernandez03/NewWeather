309ecdde6076a3fb9f65730cd50b821b222c7ec8
avfernandez03 <167803504+avfernandez03@users.noreply.github.com>
2024-04-25 20:19:52 +0200

    Injected more weather data

--- src/index.js
+++ src/index.js
@@ -2,10 +2,35 @@ function refreshWeather(response) {
     let temperatureElement= document.querySelector("#temperature");
     let temperature = response.data.temperature.current;
     let cityElement= document.querySelector("h1");
+    let descriptionElement= document.querySelector("#description");
+    let humidityElement= document.querySelector("#humidity");
+    let windElement= document.querySelector("#wind");
+    let timeElement = document.querySelector("#time");
+    let date= new Date (response.data.time * 1000);
 
-    cityElement.innerHTML= response.data.city
+    cityElement.innerHTML= response.data.city;
+    descriptionElement.innerHTML= response.data.condition.description;
+    humidityElement.innerHTML= `${response.data.temperature.humidity}%`;
+    windElement.innerHTML= `${response.data.wind.speed} km/h `;
+    timeElement.innerHTML= formatDate(date);
     temperatureElement.innerHTML=  Math.round(temperature);
+
+   
 }
+
+function formatDate(date) {
+    let day= days[date.getDay()];
+    let hours= date.getHours();
+    let minutes= date.getMinutes();
+    let days= [`Sunday`, `Monday`, `Tuesday`, `Wednesday`, `Thursday`, `Friday`, `Saturday`];
+
+    if (minutes < 10) {
+        minutes = `0${minutes}`;
+    }
+    
+    return `${day} ${hours}:${minutes}`;
+    }
+    
 
 
 
@@ -19,12 +44,10 @@ function handleSearch(event) {
     event.preventDefault();
 
     let searchInput= document.querySelector("#search-form-input");
- 
+    let cityElement= document.querySelector("h1")
     cityElement.innerHTML= searchInput.value;
     searchCity(searchInput.value);
 }
 
 let searchInputElement = document.querySelector("#search-form");
 searchInputElement.addEventListener("submit", handleSearch);
-
-searchCity("San Jose");
